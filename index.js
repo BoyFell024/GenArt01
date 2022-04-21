5be60dbffcfa6d50fb435a2233187cd9a4a8f124
BoyFell024 <nic@dubfire.co.uk>
2022-04-21 22:31:36 +0100

    1 file modified

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ import mergeImages from 'merge-images';
 
 // Write code
 
-mergeImages(['/GA_Face1.png', '/GA_Ears1.png', '/GA_Nose1.png'])
+  mergeImages(['/GA_Face1.png', '/GA_Ears1.png', '/GA_Nose1.png'])
   .then(b64 => document.querySelector('img').src = b64);
+
+  var fs = require('fs');
+  ar data = img.replace(/^data:image\/\w+;base64,/, "");
+  var buf = new Buffer(data, 'base64');
+  fs.writeFile('image.png', buf);
+
 
 
 
 // Write Javascript code!
-// const appDiv = document.getElementById('app');
-// appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-
+//const appDiv = document.getElementById('app');
+// appDiv.innerHTML = newImage;
+//appDiv.innerHTML = `<h1>JS Starter</h1>`;
