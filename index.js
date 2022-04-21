0825769c4e7231a231202b362dd1a5dd762e2a8f
BoyFell024 <nic@dubfire.co.uk>
2022-04-21 21:11:52 +0100

    1 file modified

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import mergeImages from 'merge-images';
 
 // Write code
 
-mergeImages(['/body.png', '/eyes.png', '/mouth.png'])
+mergeImages(['/GA_Face1.png', '/GA_Ears1.png', '/GA_Mouth1.png'])
   .then(b64 => document.querySelector('img').src = b64);
 
 
