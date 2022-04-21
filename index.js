411e6089029a122dce190437352467ff012c47e6
BoyFell024 <nic@dubfire.co.uk>
2022-04-21 22:50:21 +0100

    1 file modified

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ import mergeImages from 'merge-images';
 
 // Write code
 
-  mergeImages(['/GA_Face1.png', '/GA_Ears1.png', '/GA_Nose1.png'])
-  .then(b64 => document.querySelector('img').src = b64);
-
-  var fs = require('fs');
-  ar data = img.replace(/^data:image\/\w+;base64,/, "");
-  var buf = new Buffer(data, 'base64');
-  fs.writeFile('image.png', buf);
-
-
-
+//  mergeImages(['/GA_Face1.png', '/GA_Ears1.png', '/GA_Nose1.png'])
+//  .then(b64 => document.querySelector('img').src = b64);
 
 // Write Javascript code!
 //const appDiv = document.getElementById('app');
 // appDiv.innerHTML = newImage;
 //appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+import mergeImg from 'merge-img';
+ 
+mergeImg(['GA_Face1.png', 'GA_Ears1.jpg'])
+  .then((img) => {
+    // Save image as file
+    img.write('out.png', () => console.log('done'));
+  });
